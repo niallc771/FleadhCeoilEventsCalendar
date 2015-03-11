5f800bc84a4fa520a2d212b9802beacc9e99cd5f
niallc771 <s00128716@mail.itsligo.ie>
2015-03-11 05:02:40 +0000

    message

--- wp-content/plugins/ajax-event-calendar-changes/EventsMasterList.php
+++ wp-content/plugins/ajax-event-calendar-changes/EventsMasterList.php
@@ -1,45 +1,95 @@
+<script type="text/javascript" src="jquery-latest.js"></script> 
+<script type="text/javascript" src="jquery.tablesorter.js"></script> 
+
+
+	<style type="text/css">
+        table {
+            border: none;
+        }
+        th,
+        td {
+            padding: 4px 16px;
+            font-family: Times New Roman;
+            text-align: left;
+        }
+        th {
+            background-color: #C8C8C8;
+        }
+    </style>
+<script>
+//$(document).ready(function() 
+    //{ 
+	//put in window.onload
+	//alert("hi");
+	//var tbl = <?php echo json_encode($tbl)?>;
+	//var hi = "hi";
+	//alert (hi);
+	//document.appendChild(tbl);
+	
+		//$("#evnt").tablesorter(); 
+    //} 
+//); 
+
+
+
+</script>	
+	
 <?php
-    echo "<table style='border: solid 1px black;'>";
-    echo "<tr><th>User</th><th>Event</th><th>Start</th><th>Finish</th><th>Venue</th></tr>";
-    
-    class TableRows extends RecursiveIteratorIterator { // RecursiveIteratorIterator can be used to iterate through recursive iterators 
-         function __construct($display) { 
-             parent::__construct($display, self::LEAVES_ONLY); 
-         }
-    
-         function current() {
-             return "<td style='width: 350px; border: 1px solid black;'>" . parent::current(). "</td>";
-         }
-    
-         function beginChildren() { 
-             echo "<tr>"; 
-         } 
-    
-         function endChildren() { 
-             echo "</tr>" . "\n";
-         } 
-    } 
-    require_once 'config.php';
-    
-    try {
-         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);  // PDO represents a connection between PHP and a database server
-         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // PDO will throw a PDOException and set its properties to reflect the error code and error information which will  effectively blow up the code and find where the problem is
-         $stmt = $conn->prepare("SELECT wp_aec_event.user_id, wp_aec_event.title, wp_aec_event.start, wp_aec_event.end, wp_aec_venue.venue_name
-                                 FROM wp_aec_event
-                                 INNER JOIN wp_aec_venue
-                                 ON wp_aec_event.venue_id = wp_aec_venue.venue_id;");  // $stmt epresents a prepared statement
-         $stmt->execute();
-    
-         // set the resulting array to associative
-         $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); // setFetchMode - Set the default fetch mode for this statement, Returns TRUE on success or FALSE on failure. 
-    
-         foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { // RecursiveArrayIterator - This iterator allows to unset and modify values and keys while iterating over Arrays and Objects in the same way as the ArrayIterator, which allows to unset and modify values and keys while iterating over Arrays and Objects. 
-             echo $v;
-         }
-    }
-    catch(PDOException $error) {
-         echo "Error: " . $error->getMessage();
-    }
-    $conn = null;
-    echo "</table>";
+
+require_once 'config.php';
+
+  $tbl = "<table id='myTable' style='border: solid 4px violet;'>
+  <thead><tr><th id='evnt' style='background-color: violet; color: white;'>Event</th>
+			<th style='background-color: violet; color: white;'>Venue</th>
+			<th style='background-color: violet; color: white;'>Category</th>
+			<th style='background-color: violet; color: white;'>Start</th>
+			<th style='background-color: violet; color: white;'>Finish</th>
+			<th style='background-color: violet; color: white;'>Organizer</th></tr><thead>";
+	echo "$tbl";
+	
+	
+class TableRows extends RecursiveIteratorIterator { 
+     function __construct($it) { 
+         parent::__construct($it, self::LEAVES_ONLY); 
+     }
+
+     function current() {
+         return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+     }
+
+     function beginChildren() { 
+         echo "<tr>"; 
+     } 
+
+     function endChildren() { 
+         echo "</tr>" . "\n";
+     } 
+} 
+  
+try {
+     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $stmt = $conn->prepare("SELECT title, wp_aec_venue.venue_name, wp_aec_event_category.category , start, end, wp_users.user_login
+	 FROM wp_aec_event 
+	 Inner Join wp_aec_event_category on
+	 wp_aec_event_category.id = wp_aec_event.category_id
+	 Inner Join wp_users on 
+	 wp_users.id = wp_aec_event.user_id
+	 Inner Join wp_aec_venue on
+	 wp_aec_venue.venue_id = wp_aec_event.venue_id
+	 Order by title"); 
+     $stmt->execute();
+	 
+     // set the resulting array to associative
+     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+         echo $v;
+     }
+}
+catch(PDOException $e) {
+     echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
 ?>  
--- wp-content/plugins/ajax-event-calendar-changes/VenueMasterList.php
+++ wp-content/plugins/ajax-event-calendar-changes/VenueMasterList.php
@@ -1,42 +1,57 @@
-<?php
-    echo "<table style='border: solid 1px black;'>";
-    echo "<tr><th>Venue</th><th>Street</th><th>City</th><th>State</th><th>Website</th></tr>";
+<style type="text/css">
+        table {
+            border: none;
+        }
+        th,
+        td {
+            padding: 4px 16px;
+            font-family: Times New Roman;
+            text-align: left;
+        }
+        th {
+            background-color: #C8C8C8;
+        }
+    </style>
 
-    class TableRows extends RecursiveIteratorIterator { 
-         function __construct($it) { 
-             parent::__construct($it, self::LEAVES_ONLY); 
-         }
-    
-         function current() {
-             return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
-         }
-    
-         function beginChildren() { 
-             echo "<tr>"; 
-         } 
-    
-         function endChildren() { 
-             echo "</tr>" . "\n";
-         } 
-    } 
-    require_once 'config.php';
-    
-    try {
-         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-         $stmt = $conn->prepare("SELECT venue_name, street, city, state, website FROM wp_aec_venue Order By venue_name"); 
-         $stmt->execute();
-    
-         // set the resulting array to associative
-         $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-    
-         foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
-             echo $v;
-         }
-    }
-    catch(PDOException $e) {
-         echo "Error: " . $e->getMessage();
-    }
-    $conn = null;
-    echo "</table>";
+<?php
+echo "<table style='border: solid 4px violet;'>";
+  echo "<tr><th style='background-color: violet; color: white;'>Venue</th><th style='background-color: violet; color: white;'>Street</th><th style='background-color: violet; color: white;'>City</th><th style='background-color: violet; color: white;'>State</th><th style='background-color: violet; color: white;'>Website</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator { 
+     function __construct($it) { 
+         parent::__construct($it, self::LEAVES_ONLY); 
+     }
+
+     function current() {
+         return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+     }
+
+     function beginChildren() { 
+         echo "<tr>"; 
+     } 
+
+     function endChildren() { 
+         echo "</tr>" . "\n";
+     } 
+} 
+require_once 'config.php';
+
+try {
+     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $stmt = $conn->prepare("SELECT venue_name, street, city, state, website FROM wp_aec_venue Order By venue_name"); 
+     $stmt->execute();
+
+     // set the resulting array to associative
+     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+         echo $v;
+     }
+}
+catch(PDOException $e) {
+     echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
 ?>  
